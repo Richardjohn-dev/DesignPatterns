62eb7edb88d87dd486ec05b64161099e37642030
Richard John <84065964+Richardjohn-dev@users.noreply.github.com>
2022-01-09 19:26:56 +0700

    Weather Monitor Chapter 2

--- DesignPuzzle/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DesignPuzzle
-{
-    public class Class1
-    {
-    }
-}
---
+++ WeatherMonitorObserver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WeatherMonitorObserver
+{
+    class Program
+    {
+        // Chapter 2 Weather Observer Chapter
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
+
+    }
+
+    public interface IObserver
+    {
+        void Update(float temp, float humidity, float pressure);
+    }
+
+    public interface IDisplayElement
+    {
+        void Display();
+    }
+}
