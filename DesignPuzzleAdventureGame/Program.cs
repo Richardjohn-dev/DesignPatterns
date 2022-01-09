b45376bb437e551c97f8a795a605996bb76f3827
Richard John <84065964+Richardjohn-dev@users.noreply.github.com>
2022-01-09 13:26:47 +0700

    My own implementation of the puzzle game

--- DesignPuzzleAdventureGame/Program.cs
+++ DesignPuzzleAdventureGame/Program.cs
@@ -6,7 +6,82 @@ namespace DesignPuzzleAdventureGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Character archer = new Archer();
+            archer.Fight();
+            archer.SetWeaponBehaviour(new KnifeBehaviour());
+            archer.Fight();
+            archer.SetWeaponBehaviour(new SwordBehaviour());
+            archer.Fight();
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Character
+    {
+        public IWeaponBehaviour WeaponBehaviour { get; set; }
+
+        public void SetWeaponBehaviour(IWeaponBehaviour wb)
+        {
+            WeaponBehaviour = wb;
+        }
+
+        public void Fight() => WeaponBehaviour.UseWeapon();
+    }
+
+
+
+    public class Archer : Character
+    {
+        public Archer()
+        {
+            WeaponBehaviour = new BowAndArrowBehaviour();
+        }
+    }
+
+    public class Warrior : Character
+    {
+        public Warrior()
+        {
+            WeaponBehaviour = new SwordBehaviour();
+        }
+    }
+
+    public interface IWeaponBehaviour
+    {
+        void UseWeapon();
+    }
+    public class AxeBehaviour : IWeaponBehaviour
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Swings the axe..");
+        }
+    }
+
+    public class BowAndArrowBehaviour : IWeaponBehaviour
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Fires the arrows..");
+        }
+    }
+
+    public class KnifeBehaviour : IWeaponBehaviour
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("reveals the knife, a big fuck-off-shiny one.");
+        }
+    }
+
+    public class SwordBehaviour : IWeaponBehaviour
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("swings the big ass sword..");
         }
     }
 }
