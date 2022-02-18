a385496ebf13f2dcfe6af95b4a88fce31970c740
Richard John <84065964+Richardjohn-dev@users.noreply.github.com>
2022-02-18 12:09:14 +0700

    Command pattern start

---
+++ RemoteCommander/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace RemoteCommander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test remote control
+
+            SimpleRemoteControl remote = new(); // Invoker
+            Light light = new(); // reciever of the request
+            LightOnCommand lightOn = new LightOnCommand(light);
+
+
+            GarageDoor garageDoor = new(); // Receiver 2
+            GarageDoorOpenCommand openGarage = new GarageDoorOpenCommand(garageDoor);
+
+            remote.SetCommand(lightOn); // Command (lightOn) passed to Invoker (remote)
+            remote.buttonWasPressed(); // simulate
+
+
+            remote.SetCommand(openGarage); // strategy pattern?
+            remote.buttonWasPressed();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface ICommand
+    {
+         public void Execute();
+    }
+
+    public class Light
+    {
+        public void On()
+        {
+            Console.WriteLine("Lights are ON.");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Lights are OFF.");
+        }
+
+    }
+
+    public class LightOnCommand : ICommand
+    {
+        private readonly Light _light;
+
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+        public void Execute()
+        {
+            _light.On();
+        }
+    }
+
+    public class SimpleRemoteControl
+    {
+        private ICommand _buttonSlot; // slot stores the command, which controls one device.
+        public SimpleRemoteControl() {}
+
+        public void SetCommand(ICommand command) // is this strategy pattern?
+        {
+            _buttonSlot = command;
+        }
+        public void buttonWasPressed()
+        {
+            _buttonSlot.Execute();
+        }
+    }
+
+   
+
+    //
+    public class GarageDoor
+    {
+        public void Up()
+        {
+            Console.WriteLine("Garage Door is OPENING.");
+        }
+
+        public void Down()
+        {
+            Console.WriteLine("Garage Door moving CLOSING.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Garage Door STOPPED.");
+        }
+        public void LightOn()
+        {
+            Console.WriteLine("Garage Door Lights are ON.");
+        }
+        public void LightOff()
+        {
+            Console.WriteLine("Garage Door Lights are OFF.");
+        }
+    }
+
+    public class GarageDoorOpenCommand : ICommand
+    {
+        private readonly GarageDoor _garageDoor;
+
+        public GarageDoorOpenCommand(GarageDoor garageDoor)
+        {
+            _garageDoor = garageDoor;
+        }
+        public void Execute()
+        {
+            _garageDoor.Up();
+        }
+    }
+}
---
+++ SingletonPluralsight/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace SingletonPluralsight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public sealed class BadSingleton
+    {
+        private static BadSingleton _instance;
+        private static BadSingleton Instance
+        {
+            get
+            {
+                //Logger.Log("Instance called.")
+                if (_instance == null)
+                {
+                    _instance = new BadSingleton();
+                }
+                return _instance;
+            }
+        }
+        private BadSingleton()
+        {
+            // cannot be created except within this class.
+            //Logger.Log("Constructor Invoked.")
+
+        }
+    }
+
+
+    //https://www.youtube.com/watch?v=sbML3xFHRbI&t=16s
+    public sealed class SingletonLock : IDisposable
+    {
+        private bool _dispose;
+        private static volatile SingletonLock _instance;
+        //ensures that the instantiation is complete before it can be accessed further. - Helping with thread safety.
+        private static readonly object _synclock = new();
+
+        private SingletonLock()
+        { }
+
+        public static SingletonLock Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (_synclock) // this is single threaded
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonLock(); // double lock pattern not needed since c# 6 volatile fix
+                    }
+
+                }
+                return _instance;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void Dispose(bool disposing)
+        {
+            if (_dispose)
+            {
+                return;
+            }
+            if (disposing)
+            {
+                _instance = null;
+            }
+        }
+    }
+
+
+#nullable enable
+    public sealed class NullableSingleton
+    {
+        private static NullableSingleton? _instance;
+        private static NullableSingleton Instance
+        {
+            get
+            {
+                //Logger.Log("Instance called.")
+
+                return _instance ??= new NullableSingleton();
+                // only evaluates the right side of the expression, if the left side is null.
+
+            }
+        }
+        private NullableSingleton()
+        {
+            // cannot be created except within this class.
+            //Logger.Log("Constructor Invoked.")
+
+        }
+    }
+}
